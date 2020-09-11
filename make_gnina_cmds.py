d6169c86aba936eba68c42ff235f5313b5a1b8ac
Paul Francoeur <33321268+francoep@users.noreply.github.com>
2020-09-11 16:10:04 -0400

    bug fixes
    more sleight typos

--- make_gnina_cmds.py
+++ make_gnina_cmds.py
@@ -108,7 +108,7 @@ with open(args.output,'w') as outfile:
 							dock_out=out_prefix+'_'.join[args.cnn]+'_'+args.cnn_scoring+'_'+arg+val+'.sdf'
 							sent+=f' --cnn {" ".join(args.cnn)} --out {dock_out}'
 						else:
-							dock_out=out_prefix+'_crossdock_default2018_'+args.cnn_scoring+'_'+arg+val+'.sdf'
+							dock_out=out_prefix+'_'+args.cnn+'_'+args.cnn_scoring+'_'+arg+val+'.sdf'
 							sent+=f' --out {dock_out}'
 
 						#adding in the stuff for the specified argument
@@ -121,12 +121,12 @@ with open(args.output,'w') as outfile:
 		for r, l, box, out_prefix in todock:
 			sent=f'gnina -r {r} -l {l} --autobox_ligand {l} --cnn_scoring {args.cnn_scoring} --cpu 1 --seed 420'
 			if not single_cnn:
-				dock_out=out_prefix+'_'.join[args.cnn]+'_'+args.cnn_scoring+'_'+arg+val+'.sdf'
+				dock_out=out_prefix+'_'.join[args.cnn]+'_'+args.cnn_scoring+'_defaults.sdf'
 				sent+=f' --cnn {" ".join(args.cnn)} --out {dock_out}'
 			else:
-				dock_out=out_prefix+'_crossdock_default2018_'+args.cnn_scoring+'_'+arg+val+'.sdf'
+				dock_out=out_prefix+'_'+args.cnn+'_'+args.cnn_scoring+'_defaults.sdf'
 				sent+=f' --out {dock_out}'
 
 			if args.gpu:
 				sent+=' --gpu'
-			outfile.write(send+'')
+			outfile.write(sent+'\n')
