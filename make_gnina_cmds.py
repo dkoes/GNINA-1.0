12e0d641d7c2b2b03c8fbec22fec4652e8d15200
Paul Francoeur <33321268+francoep@users.noreply.github.com>
2020-09-11 15:10:30 -0400

    bug fix
    another attempted fix with f-string

--- make_gnina_cmds.py
+++ make_gnina_cmds.py
@@ -87,7 +87,7 @@ with open(args.output,'w') as outfile:
 					sent=f'gnina -r {r} -l {l} --autobox_ligand {l} --cnn_scoring {args.cnn_scoring} --cpu 1 --seed 420'
 					if args.cnn:
 						dock_out=out_prefix+'_'.join[args.cnn]+'_'+args.cnn_scoring+'_'+arg+val+'.sdf'
-						sent+=f' --cnn {' '.join(args.cnn)} --out {dock_out}'
+						sent+=f' --cnn {" ".join(args.cnn)} --out {dock_out}'
 					else:
 						dock_out=out_prefix+'_crossdock_default2018_'+args.cnn_scoring+'_'+arg+val+'.sdf'
 						sent+=f' --out {dock_out}'
