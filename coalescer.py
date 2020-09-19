04300124890f7f6ed354939fa73584e606875663
Paul Francoeur <paf46@csb.pitt.edu>
2020-09-19 15:10:52 -0400

    updated coalescer.py to make 1 csv using multiple arguments for a sweep. EX) -v 2 4 6 8  can be passed and will all be in the resulting csv. See reflected usage

--- coalescer.py
+++ coalescer.py
@@ -7,7 +7,8 @@ Script to merge all of the *.rmsds files from obrms_calc.py into 1 master csv.
 import argparse, glob, re
 
 parser=argparse.ArgumentParser(description='Merge OBRMS output files into 1 file')
-parser.add_argument('-s','--suffix',type=str, required=True, help='Suffix of files to stick together.')
+parser.add_argument('-s','--suffix',type=str, required=True, help='Suffix of files to stick together. Assumes filenames are *<SUFFIX><VALUE>.rmsds')
+parser.add_argument('-v','--values',type=str, default="",nargs='+',help='Values to be searched combined with suffix. Defaults to empty string. Accepts any number of arguments.')
 parser.add_argument('-r','--dataroot',type=str,required=True, help='Root of directories to search')
 parser.add_argument('-o','--outfilename',type=str,required=True, help='Name of output file')
 parser.add_argument('-d','--dirlist',type=str,required=True, help='File containing directory names to work on.')
@@ -25,19 +26,23 @@ with open(args.outfilename, 'w') as outfile:
 	outfile.write('tag,molids,rmsd,pocket,rec,lig\n')
 	for pocket in dirs:
 		print(pocket)
-		todo=glob.glob(root+pocket+'*'+args.suffix)
+		for val in args.values:
+			todo=glob.glob(root+pocket+'*'+args.suffix+val+'.rmsds')
 
-		for item in todo:
-			lines2write=[x.rstrip() for x in open(item).readlines()]
+			for item in todo:
+				lines2write=[x.rstrip() for x in open(item).readlines()]
 
-			for line in lines2write:
+				for line in lines2write:
 
-				if line:
-					m=re.search(r'(\S+)_PRO_(\S+)_LIG',item)
+					if line:
+						tag,molids,rmsd=line.rstrip().split()
+						if val!="":
+							tag=val
+						m=re.search(r'(\S+)_PRO_(\S+)_LIG',item)
 
-					rec=m.group(1)
-					lig=m.group(2)
-					
-					newline=line.replace(' ',',').rstrip()+','+','.join([pocket,rec,lig])+'\n'
+						rec=m.group(1)
+						lig=m.group(2)
 
-					outfile.write(newline)
+						newline=f"{tag},{molids},{rmsd},{pocket},{rec},{lig}\n"#line.replace(' ',',').rstrip()+','+','.join([pocket,rec,lig])+'\n'
+
+						outfile.write(newline)
